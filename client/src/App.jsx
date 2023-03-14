b38bc5988c882d9b3a820fbea774ffd5594701ce
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2023-03-14 16:06:45 +0530

    added Accountpage and placepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
 import {Route, Routes} from 'react-router-dom';
+import axios from 'axios';
+import { UserContextProvider } from './UserContext';
+
+import './App.css'
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import axios from 'axios';
-import { UserContextProvider } from './UserContext';
+import AccountPage from './pages/AccountPage';
 
 axios.defaults.baseURL='http://127.0.0.1:4000';
 axios.defaults.withCredentials=true;
@@ -18,6 +20,8 @@ function App() {
           <Route index element={<IndexPage/>}/>
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/account/:subpage?" element={<AccountPage/>}/>
+          <Route path="/account/:subpage/:action" element={<AccountPage/>}/>
         </Route>
       </Routes>
     </UserContextProvider>
