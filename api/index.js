fa215d2eedcc1ab37150d7f12966e6eba6a71b41
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2023-03-14 14:46:12 +0530

    updated login and register

---
+++ api/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User.js');
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const app = express();
+const bcryptSalt = bcrypt.genSaltSync(12);
+const jwtSecret='dsafhlkhwelfasfadgguoriuq';
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials:true,
+    origin:'http://127.0.0.1:5173',
+}));
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log('Mongoose Connected');
+}).catch(err=>{
+    console.log(`error is ${err}`);
+});
+
+app.get('/test',(req,res)=>{
+    res.json('test ok');
+});
+
+app.post('/register', async (req,res)=>{
+    try{
+        const {name, email, password} = req.body;  
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    }catch(err){
+        res.status(422).json(err);
+    }
+});
+
+app.post('/login', async (req,res)=>{
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if(userDoc){
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if(passOk){
+            jwt.sign({email:userDoc.email, id:userDoc._id, name:userDoc.name}, jwtSecret, {} , (err,token)=>{
+                if(err) throw err;
+                res.cookie('token',token).json(userDoc);
+            });
+        }else{
+            res.status(422).json('pass not ok');
+        }
+    }else{
+        res.status(404).json('not found');
+    }
+});
+
+app.get('/profile',(req,res)=>{
+    const {token} = req.cookies;
+    if(token){
+        jwt.verify(token, jwtSecret, {}, async (err, userData)=>{
+            if(err) throw err; 
+            const {name, email,_id} = await User.findById(userData.id);
+            res.json({name,email,_id});
+        });
+    }else{
+        res.json(null);
+    }
+})
+
+app.listen(4000, function(){
+    console.log('Server listening on 4000');
+});
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,37 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from 'axios';
+import { UserContext } from "../UserContext";
 
 export default function LoginPage () {
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    const {setUser} = useContext(UserContext);
+    async function handleLoginSubmit(event){
+        event.preventDefault();
+        try{
+            const {data} = await axios.post('/login', {email, password});
+            alert('Login Successful');
+            setUser(data);
+            setRedirect(true);
+        }catch(err){
+            console.log(err);
+            alert('Login Failed');
+        }
+    }
+
+    if(redirect){
+        return <Navigate to={'/'}/>
+    }
+
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
-                <form className="max-w-md mx-auto" action="">
-                    <input type="email" placeholder="your@email.com" />
-                    <input type="password" placeholder="password"/>
+                <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+                    <input type="email" placeholder="your@email.com" value={email} onChange={e=>setEmail(e.target.value)}/>
+                    <input type="password" placeholder="password" value={password} onChange={e=>setPassword(e.target.value)}/>
                     <button className="primary">Login</button>
                     <div className="text-center py-2 text-gray-500">
                         Don't have an account yet ?  <Link className="underline text-black" to={'/register'}>Register now</Link>
